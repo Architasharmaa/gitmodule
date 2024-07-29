ca588e203fa5ad837b7c0ad5f988228990529af4
Architasharmaa <159230390+Architasharmaa@users.noreply.github.com>
2024-07-29 17:14:21 +0530

    add files via upload

---
+++ provider.tf
@@ -0,0 +1,34 @@
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source  = "newrelic/newrelic"
+#       version = "3.37.1"
+#     }
+#   }
+# }
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source  = "newrelic/newrelic"
+#       version = "~> 3.0"   # Update this line
+#     }
+#   }
+# }
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source = "newrelic/newrelic"
+#       version = ">= 3.28.3"  
+#     }
+  
+#   }
+  
+# }
+
+
+
+
+provider "newrelic" {
+  account_id = var.account_id
+  api_key    = var.api_key
+}
